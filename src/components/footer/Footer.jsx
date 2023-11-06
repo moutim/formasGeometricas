d82f932aaec6f09961d21094f87f905d2eaba592
Vitor Moutim <moutimg@gmail.com>
2023-11-06 00:07:13 -0300

    fix

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import './footer.css';
 function Footer() {
   return (
     <footer>
-        <p>Developted by <a href="https://www.linkedin.com/in/vitormoutim/"> Vitor Moutim</a> ®</p>
+        <p>Developted by <a href="https://www.linkedin.com/in/vitormoutim/"> Vitor Moutim</a> and <a href="https://www.linkedin.com/in/camile-santana-644366202/">Camile Santana</a> ®</p>
     </footer>
   )
 }
